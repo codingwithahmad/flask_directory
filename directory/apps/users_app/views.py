7888af0c85b14a0ad173aa348485632b755820a8
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-03-05 22:49:57 +0330

    change index view to get json object and set username and password for new user

--- directory/apps/users_app/views.py
+++ directory/apps/users_app/views.py
@@ -1,6 +1,18 @@
 from . import users
+from flask import request
+from .models import User
+
+@users.route('/', methods=['POST'])
+def create_user():
+    if not request.is_json:
+        return {'error': 'JSON Only!'}, 400
+
+    args = request.get_json()
+
+    new_user = User()
+
+    new_user.username = args.get('username')
+    new_user.password = args.get('password')
 
 
-@users.route('/')
-def index():
-    return {"message": "Hello from users"}
+    return {}
