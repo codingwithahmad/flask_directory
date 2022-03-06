1250410aa73b7d3546a30c3b1bb5d5c5f68b533c
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-03-06 23:18:53 +0330

    add user to db and exception for errors in index function of users app

--- directory/apps/users_app/views.py
+++ directory/apps/users_app/views.py
@@ -1,6 +1,8 @@
 from . import users
 from flask import request
 from .models import User
+from directory import db
+from sqlalchemy.exc import IntegrityError
 
 @users.route('/', methods=['POST'])
 def create_user():
@@ -10,9 +12,17 @@ def create_user():
     args = request.get_json()
 
     new_user = User()
+    try:
+        new_user.username = args.get('username')
+        new_user.password = args.get('password')
+        db.session.add(new_user)
+        db.session.commit()
+    except ValueError as e:
+        db.session.rollback()
+        return {"Error": f'{e}' }, 400
+    except IntegrityError as e:
+        db.session.rollback()
+        return {"Error": "Username is duplicated."}
 
-    new_user.username = args.get('username')
-    new_user.password = args.get('password')
 
-
-    return {}
+    return {"message": "Account created successfully."}, 201
